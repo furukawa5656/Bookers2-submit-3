0ee0a09b047ae998606b71e48fade4ce3d30be6c
古川倖輔 <furukawa@furukawasaiwaitasukunoMacBook-Air.local>
2020-07-04 13:21:02 +0900

    Bookers2-submit3

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+
+
+
+	root 'home#top'
+
+	 devise_for :users
+	 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	 resources :users
+
+	 resources :books do
+	 	resources :book_comments, only: [:create, :destroy]
+	 	resource :favorites, only: [:create, :destroy]
+	 end
+
+	 get 'home/about'
+end
